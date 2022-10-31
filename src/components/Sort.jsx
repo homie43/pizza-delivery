fbda55badb48bcd55c0de35475b7fe33e214bf7b
Vladimir <homie43@yandex.ru>
2022-10-31 14:59:48 +0300

    sorting became dynamic

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 const Sort = () => {
+  const [open, setOpen] = React.useState(false); // popup окно
+  const [selected, setSelected] = React.useState(0);
+  const list = ["популярности", "цене", "алфавиту"];
+
+  const onClickList = (i) => {
+    setSelected(i);
+    setOpen(false);
+  };
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -17,15 +26,23 @@ const Sort = () => {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{list[selected]}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {list.map((name, i) => (
+              <li
+                key={i}
+                onClick={() => onClickList(i)}
+                className={selected === i ? "active" : ""}
+              >
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
