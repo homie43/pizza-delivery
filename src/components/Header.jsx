82e71b6232791db5b33a804f982008dd7a6a611c
Vladimir <homie43@yandex.ru>
2022-11-08 13:36:26 +0300

    add CartEmpty, CartItem, logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Search from "./Search/Search";
 
 const Header = () => {
+  // возвращаю состояние всей корзины в хедер, для отображения чсла товаров
+  const { items, totalPrice } = useSelector((state) => state.cart);
+
+  const totalCount = items.reduce((sum, item) => sum + item.count, 0);
+
   return (
     <div className="header">
       <div className="container">
@@ -20,7 +26,7 @@ const Header = () => {
 
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
-            <span>520 ₽</span>
+            <span>{totalPrice} ₽</span>
             <div className="button__delimiter"></div>
             <svg
               width="18"
@@ -51,7 +57,7 @@ const Header = () => {
                 strokeLinejoin="round"
               />
             </svg>
-            <span>3</span>
+            <span>{totalCount}</span>
           </Link>
         </div>
       </div>
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addItem } from "../redux/slices/cartSlice";
 
-const PizzaBlock = ({ imageUrl, title, price, sizes, types }) => {
+const typeNames = ["тонкое", "традиционное"];
+
+const PizzaBlock = ({ id, imageUrl, title, price, sizes, types }) => {
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) =>
+    state.cart.items.find((obj) => obj.id === id)
+  );
+  const addedCount = cartItem ? cartItem.count : 0;
   const [activeType, setActiveType] = React.useState(0);
   const [activeSize, setActiveSize] = React.useState(0);
-  const typeNames = ["тонкое", "традиционное"];
+
+  const onClickAdd = () => {
+    const item = {
+      id,
+      title,
+      price,
+      imageUrl,
+      type: typeNames[activeType],
+      size: sizes[activeSize],
+    };
+    dispatch(addItem(item));
+  };
 
   return (
     <div className="pizza-block">
@@ -35,7 +55,10 @@ const PizzaBlock = ({ imageUrl, title, price, sizes, types }) => {
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} ₽</div>
-        <button className="button button--outline button--add">
+        <button
+          onClick={onClickAdd}
+          className="button button--outline button--add"
+        >
           <svg
             width="12"
             height="12"
@@ -49,7 +72,7 @@ const PizzaBlock = ({ imageUrl, title, price, sizes, types }) => {
             />
           </svg>
           <span>Добавить</span>
-          <i>{0}</i>
+          {addedCount > 0 && <i>{addedCount}</i>}
         </button>
       </div>
     </div>
